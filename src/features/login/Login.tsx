f550df10bf9c3f131d378053c293c86e33049510
Vicky Hermawan <107031434+vhermawan@users.noreply.github.com>
2022-10-24 00:11:34 +0700

    feat: login page

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -1,7 +1,53 @@
-export default function Login(){
+import { TextInput, Checkbox, Button, Group, Box, PasswordInput, Center } from '@mantine/core';
+import { useForm } from '@mantine/form';
+
+export default function Login() {
+  const form = useForm({
+    initialValues: {
+      email: '',
+      password: '',
+      termsOfService: false,
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+    },
+  });
+
   return (
     <>
-      <div>login</div>
+      <Center sx={{ width: '700px', minHeight: '100vh', padding:'20px' }}  px="xl" mx="auto" my="auto">
+        <Box>
+          <form onSubmit={form.onSubmit((values) => console.log(values))}>
+            <TextInput
+              withAsterisk
+              label="Email"
+              placeholder="your@email.com"
+              {...form.getInputProps('email')}
+            />
+
+            <PasswordInput
+              mt="md"
+              placeholder="Password"
+              label="Password"
+              description="Password must include at least one letter, number and special character"
+              aria-label="Your password"
+              withAsterisk
+              {...form.getInputProps('password')}
+            />
+
+            <Checkbox
+              mt="md"
+              label="I agree to sell my privacy"
+              {...form.getInputProps('termsOfService', { type: 'checkbox' })}
+            />
+
+            <Group position="right" mt="md">
+              <Button type="submit">Submit</Button>
+            </Group>
+          </form>
+        </Box>
+      </Center>
     </>
-  )
+  );
 }
